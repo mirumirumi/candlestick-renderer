780dbc3cfe39057668ea5bd963a667317bf8390c
mirumi <mail@mirumi.tech>
2023-10-01 17:38:50 +0900

    Save up to base layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 
 import { AppComponent } from "./components/app.component"
+import { FooterComponent } from "./components/layouts/footer/footer.component"
+import { HeaderComponent } from "./components/layouts/header/header.component"
 import { IndexComponent } from "./pages/index/index.component"
 import { RouterModule } from "./router/routes"
 
 @NgModule({
-  declarations: [AppComponent, IndexComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, IndexComponent],
   imports: [BrowserModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent],
